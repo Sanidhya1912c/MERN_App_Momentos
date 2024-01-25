0a491467506f018707d8fdc9e6d0529cd373753e
Sanidhya1912 <sanidhyac1912@gmail.com>
2024-01-25 15:54:54 +0530

    just replace some files

--- README.md
+++
@@ -1,2 +0,0 @@
-# MERN_App_Momentos
-A mern app fully build with react and other helper dependencies 
--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Post from "./Posts/Post/Post";
-import Posts from "./Posts/Posts";
-import Form from "./Form/Form";
-
-export { Posts, Post, Form };
